e5d3c4adfaef3c12f2458ef8e8f14bdb0c4d6fb9
David Khachaturov <d77777k@gmail.com>
2020-10-05 21:43:02 +0100

    hdqn implementation

--- envs/dsdp.py
+++ envs/dsdp.py
@@ -2,15 +2,22 @@ import random
 import numpy as np
 from gym import spaces
 
+TOTAL_STATES = 6
+
 class DiscreteStochasticDecisionProcess:
     def __init__(self):
         # left or right
         self.action_space = spaces.Discrete(2)
         # 6 possible states, from 0 to 5
-        self.observation_space = spaces.Box(np.array([0]), np.array([5]), dtype=int)
+        self.observation_space = np.zeros(TOTAL_STATES)
         
         self.current_state = 1 # start in state 2
         self.visited_last = False
+    
+    def one_hot(self, n):
+        buffer = np.zeros(TOTAL_STATES)
+        buffer[n] = 1.0
+        return np.ndarray((TOTAL_STATES), buffer=buffer, dtype=np.float)
     
     # return format: current state, reward, is done, additional info
     def step(self, action):
@@ -27,11 +34,11 @@ class DiscreteStochasticDecisionProcess:
         if self.current_state == 5:
             self.visited_last = True
         if self.current_state == 0:
-            return np.ndarray((1), buffer=np.array([self.current_state]), dtype=int), 1.0 / (1.0 if self.visited_last else 100.0), True, {}
-        return np.ndarray((1), buffer=np.array([self.current_state]), dtype=int), 0.0, False, {}
+            return self.one_hot(self.current_state), 1.0 / (1.0 if self.visited_last else 100.0), True, {}
+        return self.one_hot(self.current_state), 0.0, False, {}
     
     def reset(self):
         self.current_state = 1
         self.visited_last = False
-        return np.ndarray((1), buffer=np.array([self.current_state]), dtype=int)
+        return self.one_hot(self.current_state)
                     
