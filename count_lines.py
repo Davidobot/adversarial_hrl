8b42a8ebf7cf8ee01bc7baf5fee20d6e9a1c92e1
David Khachaturov <d77777k@gmail.com>
2021-04-06 17:05:59 +0100

    - line count app

---
+++ count_lines.py
@@ -0,0 +1,25 @@
+#!/usr/bin/env python
+
+# adapted from https://stackoverflow.com/a/46132145/2580489
+from json import load
+from sys import argv
+import glob, os
+
+def loc(nb):
+    cells = load(open(nb))['cells']
+    return sum(len(c['source']) for c in cells if c['cell_type'] == 'code')
+
+def run(ipynb_files):
+    return sum(loc(nb) for nb in ipynb_files)
+
+if __name__ == '__main__':
+    #print(glob.glob("**/*.ipynb", recursive=True))
+    print("ipynb notebooks: ", run(glob.glob("notebooks/**/*.ipynb", recursive=True)))
+    print("ipynb plotting: ", run(glob.glob("plotting/**/*.ipynb", recursive=True)))
+    tot = 0
+    #print(glob.glob("*.py", recursive=True))
+    for f in glob.glob("envs/**/*.py", recursive=True):
+        tot += sum(1 for line in open(f))
+    for f in glob.glob("notebooks/**/*.py", recursive=True):
+        tot += sum(1 for line in open(f))
+    print("py: ", tot)
